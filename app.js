a708b7767a67e1d9ec4c5f2a67b06643654ae1dc
XAchtung <89976374+XAchtung@users.noreply.github.com>
2022-01-08 21:38:19 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+//Zadania
+function WinOpen_berg1(){
+    window.open("zad2.html");
+}
+function WinOpen_berg2(){
+    window.open("zad3.html");
+}
+function WinOpen_berg3(){
+    window.open("zad4.html");
+}
+function WinOpen_berg4(){
+    window.open("strona_responsywna_berg/index_berg.html");
+}
+//Kody
+function WinOpen_berg1_kod(){
+    window.open("zad2_kod.html");
+}
+function WinOpen_berg2_kod(){
+    window.open("zad3_kod.html");
+}
+function WinOpen_berg3_kod(){
+    window.open("zad4_kod.html");
+}
